127c2bdfbba31cc03489bbc7a318ebd567b0e6b6
Gungundamdam <holimelonapp@gmail.com>
2025-10-21 23:04:10 +0200

    wip shadcn

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect, useRef } from 'react';
+import { IconRefresh, IconSun, IconMoon } from '@tabler/icons-react';
+
+import { useApp, AppProvider } from './context/AppContext';
+
+import { useTheme } from './context/ThemeContext';
+
+import RealTime from './components/RealTime';
+import { Button } from './components/ui/button';
+
+
+function App() {
+
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <div className="App max-w-7xl mx-auto p-4">
+
+      {/* <Button
+        variant="outline"
+        size="icon"
+        onClick={toggleTheme}
+        title={theme === 'light' ? 'Passer au mode sombre' : 'Passer au mode clair'}
+      >
+        {theme === 'light' ? (
+          <IconMoon className="h-4 w-4" />
+        ) : (
+          <IconSun className="h-4 w-4" />
+        )}
+      </Button> */}
+      <AppProvider>
+        <RealTime />
+      </AppProvider>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import 'uplot/dist/uPlot.min.css';
+
+import { ThemeProvider } from './context/ThemeContext';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+
+      <App />
+    
+    </ThemeProvider>
+  </React.StrictMode>
+);
